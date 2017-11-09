4702b0f4490f01fda2cf88d98000a8f443778fef
mrTech <shamot.group@gmail.com>
2017-11-09 14:20:45 +0330

    counter added

--- textAnalyser.js
+++ textAnalyser.js
@@ -6,3 +6,21 @@ var parser=function(text){
     
 
 }
+
+var counter=function(words){
+    var results=[];
+    words.forEach(function(word){
+        var found=false;
+        results.forEach(function(result){
+            if(results.word==word){
+                found=true;
+                results.count++;
+                break;
+            }
+        })
+        if(!found){
+            results.push({word:word,count:1});
+        }
+    });
+    return results;
+}
