8a22355b91677bb1a7822609d808f7b9d238c4e8
shtahvildary <shtahvildary@gmail.com>
2017-11-10 00:15:03 +0330

    Parser is added

--- textAnalyser.js
+++ textAnalyser.js
@@ -1,25 +1,60 @@
-module.exports=function(text,callback){
+module.exports = function (text, callback) {
+    var words = parser(text);
+    var counts = counter(words);
+    console.log(words);
+    console.log(counts);
+
+
 
 }
 
-var parser=function(text){
-    
+var parser = function (text) {
+    var seprators = [' ', '/', '.', ',', '?', '>', '<', '""', "''", ';', ':', '\\', ']', '[', '}', '{', '+', '=', '-', '_', ')', '(', '*', '&', '^', '%', '$', '#', '@', '!', '~', '`', '\n', '\t'];
+
+    //todo: multiple space should be handeled
+    seprators.forEach(function (char) {
+        text = text.replace(char, " ");
+    })
+    if (text.endsWith(" ")) { //so the last char is " " and should be removed
+        text = text.slice(0, text.length - 1);
+    }
+    var words = [];
+    var length = text.lastIndexOf(" ");
+    while (text.lastIndexOf(" ") != -1) {
+        if(text.charAt(text.indexOf(" ")+1)!=" "){
+        words.push(text.slice(0, text.indexOf(" ")));
+        
+        text = text.slice(text.indexOf(" ") + 1);
+        }
+        else{
+//            words.push(text.slice(0, text.indexOf(" ")));
+            
+            text = text.slice(text.indexOf(" ") + 2);
+        }
+    }
+    words.push(text);
+    // console.log(words);
+    return words;
+
 
 }
 
-var counter=function(words){
-    var results=[];
-    words.forEach(function(word){
-        var found=false;
-        results.forEach(function(result){
-            if(results.word==word){
-                found=true;
+var counter = function (words) {
+    var results = [];
+    words.forEach(function (word) {
+        var found = false;
+        results.every(function (result) {
+            if (results.word == word) {
+                found = true;
                 results.count++;
-                break;
+
             }
         })
-        if(!found){
-            results.push({word:word,count:1});
+        if (!found) {
+            results.push({
+                word: word,
+                count: 1
+            });
         }
     });
     return results;
