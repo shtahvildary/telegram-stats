57c4168d5425026c4ccb319a821949d4bbcdbf17
shtahvildary <shtahvildary@gmail.com>
2017-11-11 21:56:15 +0330

    some corrections in messages

--- Schema/Messages.js
+++ Schema/Messages.js
@@ -2,7 +2,12 @@ const mongoose=require("mongoose");
 
 var messageSchema=mongoose.Schema({
     chatId:String,
+    chatTiltle:String,
+    type:String,
     message:String,
-    date:{type:Date,default:Date.now}
+    date:{type:Date,default:Date.now},
+    keywords:[String]
 })
+
+
 module.exports=mongoose.model("messages",messageSchema);
--- app.js
+++ app.js
@@ -1,22 +1,52 @@
-var messageDB=require("./Schema/Messages");
-const botgram=require("botgram");
-const db=require("./config/DBConfig");
+var messageDB = require("./Schema/Messages");
+const botgram = require("botgram");
+const db = require("./config/DBConfig");
 
-var textAnalyser=require('./textAnalyser')("Some Text       Here! به همه دوستان ولی ... سلام");
+var textAnalyser = require('./textAnalyser')("Some Text       Here! به همه دوستان ولی ... سلام");
 
-var bot=botgram("456299862:AAGB1q_AMolsLpeE5EARolW4FHEi5-1kqjE");
-bot.command("start",function(msg,reply){
+var bot = botgram("456299862:AAGB1q_AMolsLpeE5EARolW4FHEi5-1kqjE");
+bot.command("start", function (msg, reply) {
     console.log(msg);
     reply.text("salammmmm");
 })
 
-bot.text(function(msg,reply){
+bot.text(function (msg, reply) {
 
     console.log(msg);
-    var newMessage=new messageDB({
-        chatId:msg.chat.id,
-        message:msg.text
+    var newMessage = new messageDB({
+        type: msg.type,
+        chatId: msg.chat.id,
+        chatTitle: msg.chat.title,
+        message: msg.text,
+        keywords: msg.hashtags()
     })
     newMessage.save();
     //reply.text("salammmmm");
 })
+bot.video(function (msg, reply, next) {
+
+})
+bot.audio(function (msg, reply, next) {
+
+})
+bot.voice(function (msg, reply, next) {
+
+})
+bot.contact(function (msg, reply, next) {
+
+})
+bot.image(function (msg, reply, next) {
+
+})
+bot.document(function (msg, reply, next) {
+
+})
+bot.photo(function (msg, reply, next) {
+
+})
+bot.sticker(function(msg,reply,next){
+    
+    })
+    bot.video(function(msg,reply,next){
+        
+        })
