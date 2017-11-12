770fc9d44101a3f9ee4fd09d774ee002f5574243
mrTech <shamot.group@gmail.com>
2017-11-13 00:36:47 +0330

    api fixed

--- js/api.js
+++ js/api.js
@@ -1,16 +1,18 @@
 (function ($) {
     $(function () {
-        var post = function (endpoint, data) {
-            $
-                .ajax({
-                    method: "POST",
-                    url: "localhost:3000" +endpoint,
-                    data: data
-                })
-                .done(function (msg) {
-                    console.log("Data Saved: " + msg);
-                });
-        }
+        
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+var post = function (endpoint, data,callback) {
+    $
+        .ajax({
+            method: "POST",
+            url: "localhost:5001" +endpoint,
+            data: data
+        })
+        .done(function (msg) {
+            callback(msg)
+        });
+}
