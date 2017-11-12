89bd3b580b7bef3de16b4beb36d7fe89f6f8198f
mrTech <shamot.group@gmail.com>
2017-11-12 10:33:23 +0330

    initial

--- config/DBConfig.js
+++
@@ -1,9 +0,0 @@
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/TelegramStats');
-var db=mongoose.connection;
-db.on('error',console.error.bind(console,'connection error!'));
-db.once('open',function(){
-  console.log("DB is connected...LET'S GO!");
-});
-
-module.exports=db;
