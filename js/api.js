64be8d013736befdb8d6cdd2a5e6459571a531f1
mrTech <shamot.group@gmail.com>
2017-11-13 13:16:35 +0330

    x-access-token

--- js/api.js
+++ js/api.js
@@ -10,7 +10,8 @@ var post = function (endpoint, data,callback) {
         .ajax({
             method: "POST",
             url: "http://178.33.79.204:5001" +endpoint,
-            data: data
+            data: data,
+            headers:{'x-access-token':$.cookie("token")}
         })
         .done(function (msg) {
             console.log(msg)
