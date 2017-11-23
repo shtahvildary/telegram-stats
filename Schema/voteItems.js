07ec050a0a6476f63dc22dd43c117be3e776a714
shtahvildary <shtahvildary@gmail.com>
2017-11-23 10:36:42 +0330

    channelId is added to voteItems schema

--- Schema/voteItems.js
+++ Schema/voteItems.js
@@ -2,7 +2,11 @@ const mongoose=require("mongoose");
 
 var voteItemSchema=mongoose.Schema({
     title:String,    
-    type:Number,//0:channel , 1:program    
+    type:Number,//0:channel , 1:program  
+    
+    //channels
+    channelId:String,
+
     //programs
     personnels:[{type:String}],
     description:String
