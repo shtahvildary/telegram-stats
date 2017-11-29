7062ba186a59b74dbb0bec1a30d2072561af860e
shtahvildary <shtahvildary@gmail.com>
2017-11-29 14:26:01 +0330

    voteItems update and delete is added

--- Schema/voteItems.js
+++ Schema/voteItems.js
@@ -3,12 +3,13 @@ const mongoose=require("mongoose");
 var voteItemSchema=mongoose.Schema({
     title:String,    
     type:Number,//0:channel , 1:program  
+    description:String,
+    
     
     //channels
     channelId:String,
 
     //programs
     personnels:[{type:String}],
-    description:String
 });
 module.exports=mongoose.model("voteItems",voteItemSchema);
--- routes/voteItems.js
+++ routes/voteItems.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var voteItem_sc=require("../Schema/voteItems");
+var voteItem_sc = require("../Schema/voteItems");
 
 
 //Add new program or channel
@@ -9,11 +9,11 @@ router.post('/new', function (req, res) {
   var voteItem = new voteItem_sc(req.body);
   console.log(req.body);
   voteItem.save(function (err, result) {
-    
+
     if (!err) {
 
       res.json({
-        voteItem: result  
+        voteItem: result
       });
     } else {
 
@@ -34,7 +34,7 @@ router.post('/all', function (req, res) {
         });
       } else {
         res.json({
-          error: 'There is no cannel or program to select...'
+          error: 'There is no channel or program to select...'
         });
       }
     } else {
@@ -44,6 +44,82 @@ router.post('/all', function (req, res) {
     }
   })
 })
+
+// //Search voteItems
+// router.post('/search', function (req, res) {
+//   console.log('query', req.body.query)
+//   voteItem_sc.find({
+//     "title": {
+//       $regex: req.body,
+//       $options: 'i'
+//     }
+//   }).sort('-date').exec(function (err, result) {
+//     if (!err) {
+//       res.status(200).json({
+//         voteItems: result
+//       });
+//     } else {
+//       res.status(500).json({
+//         error: err
+//       });
+//     }
+//   })
+// })
+
+
+
+//update voteItems (by id)
+router.post('/update',function(req,res){
+  console.log('query:',req.body)
+  voteItem_sc.findById(req.body._id).exec(function(err,result){
+    if(!err){
+      console.log("voteItem:",result)
+      result.title=req.body.title||result.title;
+      result.type=req.body.type||result.type;
+      result.description=req.body.description||result.description;
+      result.channelId=req.body.channelId||result.channelId;
+      result.personnels=req.body.personnels||result.personnels;
+
+      // Save the updated document back to the database
+      result.save(function(err,result){
+        if(!err){
+          res.status(200).send(result);
+        }
+        else{
+          res.status(500).send(err)
+        }
+      })
+    }else {
+      res.status(500).json({
+        error: err
+      });
+    }
+  })
+})
+
+
+
+
+
+//delete voteItems (by id)
+//URL: localhost:5001/voteItems/delete
+//INPUT:{"_id":"5a1e711ed411741d84d10a29"}
+
+router.post('/delete', function (req, res) {
+  console.log('query', req.body)
+    voteItem_sc.findByIdAndRemove(req.body._id).exec(function (err, result) {
+    if (!err) {
+      res.status(200);
+      console.log('selected voteItem is deleted!!!!');
+    } else {
+      res.status(500).json({
+        error: err
+      });
+    }
+  })
+})
+
+
 
 
 module.exports = router;
