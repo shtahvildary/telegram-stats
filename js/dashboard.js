12987b392a924423651001adede1b08d158e661b
shtahvildary <shtahvildary@gmail.com>
2017-12-29 22:09:31 +0330

    pass cookie for auth

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,7 +2,8 @@
     
     console.log('hey')
     $(function () {
-        post('/messages/select/last/date',{},function(response){
+        post('/messages/select/last/date',{token:$.cookie("token")},function(response){
+            console.log({token:$.cookie("token")})
             console.log('last messages',response)
             response.messages.map(function(item){
             $('#last-messages').append(`<li class="collection-item">
